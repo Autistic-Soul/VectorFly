99f10e193f46e0becb676544b7401c39831cb4d0
sandyzikun <36362203+sandyzikun@users.noreply.github.com>
2018-10-15 12:31:56 +0800

    Update Version-0.1.0.py

--- DL/Version-0.1.0.py
+++ DL/Version-0.1.0.py
@@ -1,87 +1,100 @@
 #!/usr/bin/env Python
 # -*- coding: utf-8 -*-
-
 import cmath
+NUMTYPES = [int, complex, float]
 
-class nerual(object):
+# Activation-Functions
 
-    """
-    weights:
-    ===
-        <list>: <int> or <float> or <complex>
+def Sigmoid(x):
+    return ( 1 / ( 1 + cmath.exp(-x) ) )
 
-    bias:
-    ===
-        <int> or <float> or <complex>
+def SinH(x):
+    return ( ( cmath.exp(x) - cmath.exp(-x) ) / 2 )
 
-    activation_function:
-    ===
-        <str>: "SIGMOID" or "TANH" ( or "TH" ) or "RELU"
-    """
+def sh(x):
+    return SinH(x)
 
-    def __init__( self, weights = None, bias = None, activation_function = "TANH" ):
-        self.__W = weights
-        self.__B = bias
-        self.__fun = activation_function
+def CosH(x):
+    return ( ( cmath.exp(x) + cmath.exp(-x) ) / 2 )
 
-    def Sigmoid(self, x):
-        return 1 / ( 1 + cmath.exp(-x) )
+def ch(x):
+    return CosH(x)
 
-    def tanh(self, x):
-        return ( cmath.exp(x) - cmath.exp(-x) ) / ( cmath.exp(x) + cmath.exp(-x) )
+def TanH(x):
+    return ( SinH(x) / CosH(x) )
 
-    def th(self, x):
-        return self.tanh(x)
+def th(x):
+    return ( sh(x) / ch(x) )
 
-    def ReLU(self, x):
-        return max(0, x)
+def ReLU(x):
+    return max(0, x)
 
-    def Compute( self, X = None ):
+# Nerual
 
-        if len(self.__W) != len(X):
+class Nerual(object):
+
+    def __init__( self, w = [ 0, 0 ] ):
+        for each in w:
+            if not ( type(each) in NUMTYPES ):
+                raise Exception()
+        self.W = w
+
+    def compute( self, X = [0], Activation_Function = None ):
+
+        if len(self.W) != ( len(X) + 1 ):
             raise Exception()
 
-        if self.__fun == "SIGMOID":
-            return self.Sigmoid(sum([ self.__W[i] * X[i] for i in range(len(self.__W)) ]) + self.__B)
+        y = self.W[0]
+        self.W.pop(0)
+        y += sum([ ( X[i] * self.W[i] ) for i in range(len(X)) ])
 
-        elif self.__fun in [ "TANH", "TH" ]
-            return self.th(sum([ self.__W[i] * X[i] for i in range(len(self.__W)) ]) + self.__B)
+        if Activation_Function == "SIGMOID":
+            return Sigmoid(y)
 
-        elif self.__fun == "RELU":
-            return self.ReLU(sum([ self.__W[i] * X[i] for i in range(len(self.__W)) ]) + self.__B)
+        elif Activation_Function in [ "TANH", "TH" ]:
+            return th(y)
+
+        elif Activation_Function == "RELU":
+            return ReLU(y)
 
         else:
-            raise Exception()
+            return y
 
-class layer(object):
+# Layer
 
-    """
-    neruals:
-    ===
-        <list>: <nerual>
+class Layer(object):
 
-    connect_type:
-    ===
-        <str>: "FULLY_CONNECT" or "SPARSE_CONNECT"
-    """
+    def __init__( self, Nerual_Nodes = [ Nerual() ] ):
+        self.nodes = Nerual_Nodes
 
-    def __init__( self, neruals = None, connect_type = "FULLY_CONNECT", sparse_connection_radius = None ):
-        self.__nodes = neruals
-        self.__connection = connect_type
-        self.__radius = sparse_connection_radius
+    def compute( self, X = [0], Activation_Function = None ):
+        return [ self.nodes[i].compute( X = X, Activation_Function = Activation_Function ) for i in range(len(self.nodes)) ]
 
-    def Compute( self, X ):
+# Net
 
-        if self.__connection == "FULLY_CONNECT":
-            return [ each_nerual.Compute(X) for each_nerual in self.__nodes ]
+class Net(object):
 
-        elif self.__connection == "SPARSE_CONNECT":
-            raise Exception()
+    def __init__( self, Layer_Nodes = [ Layer() ] ):
+        self.nodes = Layer_Nodes
 
-        else:
-            raise Exception()
+    def compute( self, X = [0], Activation_Function = None ):
+        for i in range(len(self.nodes)):
+            print( "X =", X )
+            X = self.nodes[i].compute( X = X, Activation_Function = Activation_Function )
+        print( "X =", X )
+        return X
 
-class net(object):
 
-    def __init__( self, layers = None ):
-        self.__lays = layers
+"""  TESTING PART  """
+"""  XOR  PROBLEM  """
+
+n00 = Nerual( w = [ (-0.5), 1, (-1) ] )
+n01 = Nerual( w = [ (-0.5), (-1), 1 ] )
+n10 = Nerual( w = [ 0.5, 1, 1 ] )
+
+l0 = Layer( Nerual_Nodes = [ n00, n01 ] )
+l1 = Layer( Nerual_Nodes = [ n10 ] )
+
+nn = Net( Layer_Nodes = [ l0, l1 ] )
+
+nn.compute(X = [ 1, 0 ])
